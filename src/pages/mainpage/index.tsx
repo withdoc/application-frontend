e62f126cc9ce8bc28d6cf283fba21b66c8c402fe
htchoi1006 <htchoi1006@kookmin.ac.kr>
2022-11-07 16:12:25 +0900

    mainpage 수정중

--- src/pages/mainpage/index.tsx
+++ src/pages/mainpage/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styled from './styles';
+
+import DocumentCard from '../../components/DocumentCard';
 
 
 
 function Mainpage() {
+    const [isModal, setIsModal] = useState(false);
+
+
+    const showModal = () => {
+        setIsModal(true);
+    };
+
+
+    const Modal = () => {
+        return (
+            <styled.Modal>
+                <styled.ModalContainer>
+                    <styled.CloseButton onClick={() => setIsModal(false)} />
+                </styled.ModalContainer>
+            </styled.Modal>
+        )
+    }
+
     return (
         <styled.Container>
+            {isModal && <Modal />}
+
             <styled.MainBox>
                 <styled.Header>
                     <styled.SearchIcon />
                     <styled.SearchInputBox>
                         <span>Search for anything you want</span>
                     </styled.SearchInputBox>
-                    <styled.SettingIcon />
+                    <styled.SettingIcon onClick={showModal} />
                 </styled.Header>
+                <styled.PageTitle>document management system</styled.PageTitle>
+                <styled.DocumentList>
+                    <styled.AddDocument>
+                        <styled.AddButton />
+                    </styled.AddDocument>
+                    <DocumentCard />
+                    <DocumentCard />
+                    <DocumentCard />
+                    <DocumentCard />
+                    <DocumentCard />
+                </styled.DocumentList>
             </styled.MainBox>
         </styled.Container>
     );
--- src/pages/mainpage/styles.tsx
+++ src/pages/mainpage/styles.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import searchicon from "../../imgs/searchicon.png";
 import settingicon from "../../imgs/settingicon.png";
+import addbutton from "../../imgs/addbtn.png";
+import closebutton from "../../imgs/closebutton.png";
 
 export const Container = styled.div`
     display: flex;
@@ -29,7 +31,7 @@ export const MainBox = styled.div`
     display: flex;
     flex-direction: column;
     // justify-content: center;
-    align-items: center;
+    // align-items: center;
 `;
 
 export const Header = styled.div`
@@ -65,14 +67,89 @@ export const SettingIcon = styled.img.attrs({
     width: 30px;
     height: 30px;
     margin-left: 1187px;
+    cursor: pointer;
 `;
 
 export const PageTitle = styled.div`
     font-family: Poppins;
     font-size: 48px;
     font-weight: 400;
-    line-height: 72px;
-    letter-spacing: 0px;
     text-align: left;
+    color: #14243B;
+    margin-top: 40px;
+    margin-left: 81px;
+`;
 
+
+export const DocumentList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 80px;
+    padding: 0 74px 0 74px;
+    justify-content: space-around;
+    margin-top: 60px;
+    row-gap: 36px;
+`;
+
+export const AddDocument = styled.div`
+    height: 276px;
+    width: 489px;
+    border-radius: 20px;
+    border: 1px solid #D8D8D8
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+`;
+
+export const AddButton = styled.img.attrs({
+    src: addbutton,
+})`
+    width: 64px;
+    height: 64px;
+    margin-top: 103px;
+    margin-left: 212.6px;
+`;
+
+
+// 모달창 구현 -------------------------------------
+
+export const Modal = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(112, 112, 112, 0.5);
+`;
+
+export const ModalContainer = styled.div`
+    width: 337px;
+    height: 911px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    // box-shadow: 3px 3px 5px #00000029;
+    border: 0.25px solid #B7B7B7;
+    border-radius: 30px;
+    opacity: 1;
+    // z-index: 999;
+    position: fixed;
+    right: 66px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    // justify-content: center;
+`;
+
+export const CloseButton = styled.img.attrs({
+    src: closebutton,
+})`
+    width; 40px;
+    height: 40px;
+    margin-top: 20px;
+    margin-left: 260px;
+    cursor: pointer;
 `;
