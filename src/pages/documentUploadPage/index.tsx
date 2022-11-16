7204426d5f5d13ade1788efec6376b0459c15fb3
dan-2ee <ekdms7273@naver.com>
2022-11-16 17:15:48 +0900

    make document upload component

---
+++ src/pages/documentUploadPage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./style"
+
+function DocumentUploadComponent() {
+    return (
+        <div></div>
+    )
+}
+
+export default DocumentUploadComponent;
