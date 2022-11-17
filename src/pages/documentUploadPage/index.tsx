9c74cb188c6a78b239cb7c3ef6e2a53630b3c383
dan-2ee <ekdms7273@naver.com>
2022-11-17 22:45:50 +0900

    folder component

--- src/pages/documentUploadPage/index.tsx
+++ src/pages/documentUploadPage/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import * as S from "./style"
+import profile from "../../imgs/bubbleProfile.svg";
 
 function DocumentUploadPage() {
     return (
         <>
         <S.mainContainer>
             <S.folderContainer>
-                
+            <S.profileImg src={profile}/>   
             </S.folderContainer>
         </S.mainContainer>
         </>
--- src/pages/documentUploadPage/style.tsx
+++ src/pages/documentUploadPage/style.tsx
@@ -1,20 +1,28 @@
 import styled from "styled-components";
 
-
 export const mainContainer = styled.div`
     width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     //justify-content: center;
     //align-items: center;
-    background: linear-gradient(141.8deg, #ECEEF8 2.92%, #CCCFDE 111.13%);
+    background: #FFFFFF;
 `;
 
 export const folderContainer = styled.div`
-    width: 30%;
+    width: 20%;
     height: 100%;
     background: #102141;
-    border-radius: 30px;
+    border-radius: 0 30px 30px 0;
     min-height: 100%;
-    min-width: 30%;
+    display: flex;
+    flex-direction: column;
+    min-width: 20%;
+    align-items: center;
+`
+
+export const profileImg = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-top: 10%;
 `
