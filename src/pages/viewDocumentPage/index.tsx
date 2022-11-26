912610cd3f8a8ce475d46c645174fd39a782581d
dan-2ee <ekdms7273@naver.com>
2022-11-27 03:13:38 +0900

    헤더 구현

--- src/pages/viewDocumentPage/index.tsx
+++ src/pages/viewDocumentPage/index.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./style"
 
 function ViewDocumentPage() {
+    const [documentTitle, setDocumentTitle] = useState<string>("휴학증명서.pdf");
+    const navigate = useNavigate();
+    
+    
     return (
-        <></>
+        <S.mainContainer>
+            <S.header>
+                <S.btnBack onClick={() => navigate(-1)}></S.btnBack>
+                <S.documentTitle>{documentTitle}</S.documentTitle>
+                <S.btnBox>
+                    <S.btnPrint>프린트</S.btnPrint>
+                    <S.btnDownload>다운로드</S.btnDownload>
+                </S.btnBox>
+            </S.header>
+            <S.viewContainer></S.viewContainer>
+        </S.mainContainer>
     )
 }
 
--- src/pages/viewDocumentPage/style.tsx
+++ src/pages/viewDocumentPage/style.tsx
@@ -1,3 +1,88 @@
 import styled from "styled-components";
+import back from "../../imgs/backbtn.svg";
 
-export const mainContainer = styled.div``;
+export const mainContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    background: #FFFFFF;
+`;
+
+export const header = styled.div`
+    width: 100%;
+    height: 110px;
+    position: fixed;
+    background: #FFFFFF;
+    display: flex;
+    align-items: center;
+`;
+
+export const documentTitle = styled.div`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 27px;
+    line-height: 40px;
+    color: #000000;
+    margin-left: 2%;
+`;
+
+export const btnBack = styled.img.attrs({src: back})`
+    width: 35px;
+    height: 35px;
+    margin-left: 3%;
+    cursor: pointer;
+`
+
+export const btnBox = styled.div`
+    width: 80%;
+    float: right;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+`
+
+export const btnPrint = styled.button`
+    width: 200px;
+    height: 70px;
+    background: #C4C8D0;
+    border-radius: 30px;
+    border: none;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 38px;
+    text-align: center;
+
+    color: #FFFFFF;
+    cursor: pointer;
+
+    margin-right: 3%;
+`
+
+export const btnDownload = styled.button`
+    width: 200px;
+    height: 70px;
+    background: #102141;;
+    border-radius: 30px;
+    border: none;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 38px;
+    text-align: center;
+
+    color: #FFFFFF;
+    cursor: pointer;
+`
+
+
+export const viewContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background: rgba(91, 99, 169, 0.1);
+`;
