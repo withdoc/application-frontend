214942c2d66edbc161d55e513ec097f7dc3d8daf
dan-2ee <ekdms7273@naver.com>
2022-12-01 20:42:18 +0900

    feat: 비밀번호 확인 기능 구현

--- src/pages/signupPage/index.tsx
+++ src/pages/signupPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import * as S from "./style"
 import axios from 'axios';
 import Calendar from 'react-calendar';
@@ -14,9 +14,40 @@ function SignUpComponent() {
     const [gender, setGender] = useState<string>("");
     const [nation, setNation] = useState<string>("");
 
+    const [pwCheck, setPwCheck] = useState<boolean>(true);
+
         // new Date("2020-12-28").
         //
+
+    const changeValue = (name: string) => (e: any) => {
+        switch(name) {
+            case "email": setEmail(e.target.value); break;
+            case "password": setPassword(e.target.value); break;
+            case "repeatPassword": {
+                if (password !== e.target.value) setPwCheck(false); 
+                if (password === e.target.value) setPwCheck(true); 
+                break;
+            }
+            
+        }
+    }
+
     
+    //const PwRepeatInput:any = useRef();
+    
+    const SignUpCheck = () => {
+        /*if (password === repeatPassword) {
+            setPwCheck("true");
+            PwRepeatInput.value = password;
+        }
+        if (password !== repeatPassword) {
+            setPwCheck("false");   // 비밀번호 맞지 않으면 
+            PwRepeatInput.current.focus()   // repeat password 창으로 포커스 이동 
+        }*/
+
+        // else SignUp();   // 모두 통과하면 
+    }
+
     const SignUp = () => {
         const response = axios.post(
             "http://15.164.231.10/user/signup",
@@ -43,7 +74,7 @@ function SignUpComponent() {
             console.log(error);
         })
     }
-
+    
     return (
         <S.mainContainer>
             <S.mainContent>
@@ -51,27 +82,27 @@ function SignUpComponent() {
                     Sign Up
                 </S.signUpTitle>
                 <S.inputContainer>
-                    <S.inputTitle>Email *</S.inputTitle>
-                    <S.inputBox type="email"/>
-                    <S.inputTitle>Password *</S.inputTitle>
-                    <S.inputBox type="password"/>
-                    <S.inputTitle>Repeat Password *</S.inputTitle>
-                    <S.inputBox type="password"/>
-                    <S.inputTitle>Name *</S.inputTitle>
-                    <S.inputBox type="name"/>
-                    <S.inputTitle>Birth *</S.inputTitle>
+                    <S.inputTitle check={true}>Email *</S.inputTitle>
+                    <S.inputBox check={true} type="email" onChange={changeValue("email")}/>
+                    <S.inputTitle check={true}>Password *</S.inputTitle>
+                    <S.inputBox check={true} type="password" onChange={changeValue("password")}/>
+                    <S.inputTitle check={pwCheck}>Repeat Password *</S.inputTitle>
+                    <S.inputBox check={pwCheck} type="password" onChange={changeValue("repeatPassword")}/>
+                    <S.inputTitle check={true}>Name *</S.inputTitle>
+                    <S.inputBox check={true} type="name"/>
+                    <S.inputTitle check={true}>Birth *</S.inputTitle>
                     <Calendar className="react-calendar"/>
-                    <S.inputTitle>Address *</S.inputTitle>
-                    <S.inputBox type="address"/>
-                    <S.inputTitle>Gender *</S.inputTitle>
+                    <S.inputTitle check={true}>Address *</S.inputTitle>
+                    <S.inputBox check={true} type="address"/>
+                    <S.inputTitle check={true}>Gender *</S.inputTitle>
                     <S.genderSelectBox value={gender}>
                         <option value={"female"} key={"female"}>female</option>
                         <option value={"male"} key={"male"}>male</option>
                     </S.genderSelectBox>
-                    <S.inputTitle>Nation *</S.inputTitle>
-                    <S.inputBox type="nation"/>
+                    <S.inputTitle check={true}>Nation *</S.inputTitle>
+                    <S.inputBox check={true} type="nation"/>
                 </S.inputContainer>
-                <S.btnSignUp onClick={SignUp}>Sign Up</S.btnSignUp>
+                <S.btnSignUp onClick={SignUpCheck}>Sign Up</S.btnSignUp>
             </S.mainContent>
         </S.mainContainer>
     )
--- src/pages/signupPage/style.tsx
+++ src/pages/signupPage/style.tsx
@@ -40,21 +40,23 @@ export const inputContainer = styled.div`
     flex-direction: column;
 `
 
-export const inputTitle = styled.div`
+export const inputTitle = styled.div<{check: boolean}>`
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
     font-size: 20px;
     line-height: 30px;
-    color: rgba(20, 36, 59, 0.35);
+    color: ${(props) =>
+    props.check === true ? "rgba(20, 36, 59, 0.35)" :  "rgba(237, 53, 53, 0.5)"};
     margin-top: 5%;
     margin-left: 2%;
 `
 
-export const inputBox = styled.input`
+export const inputBox = styled.input<{check: boolean}>`
     width: 100%;
     height: 45px;
-    border: 1px solid #D8D8D8;
+    border: ${(props) =>
+    props.check === true ? "1px solid #D8D8D8" : "1px solid #e04040"};
     border-radius: 10px;
     font-family: 'Poppins';
     font-style: normal;
@@ -64,6 +66,33 @@ export const inputBox = styled.input`
     color: rgba(20, 36, 59, 0.7);
     margin-top: 2%;
 `
+
+export const invalidTitle = styled.div`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 30px;
+    color: #e04040;
+    opacity: 0.7;
+    margin-top: 5%;
+    margin-left: 2%;
+`
+
+export const invalidBox = styled.input`
+    width: 100%;
+    height: 45px;
+    border: 1px solid #e04040;
+    border-radius: 10px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 36px;
+    color: rgba(20, 36, 59, 0.7);
+    margin-top: 2%;
+`
+
 export const birthInputContainer = styled.div`
     display: flex;
     flex-direction: row;
