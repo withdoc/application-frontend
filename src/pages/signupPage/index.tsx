197850f6a83dbcc1b20c5b8b5e388e8fc1bc94ec
dan-2ee <ekdms7273@naver.com>
2022-12-02 19:26:56 +0900

    feat: 회원가입 기능 구현

--- src/pages/signupPage/index.tsx
+++ src/pages/signupPage/index.tsx
@@ -4,11 +4,13 @@ import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendar.css';
+import dayjs from 'dayjs';
 
 function SignUpComponent() {
     // parameter state
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [repeatPw, setrepeatPw] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [birth, setBirth] = useState<Date>(new Date());
     const [address, setAddress] = useState<string>("");
@@ -18,7 +20,7 @@ function SignUpComponent() {
     // check state
     const [emailCheck, setEmailCheck] = useState<boolean>(true);
     const [pwCheck, setPwCheck] = useState<boolean>(true);
-    const [isSame, setIsSame] = useState<boolean>(true);
+    const [repeatCheck, setRepeatCheck] = useState<boolean>(true);
     const [nameCheck, setNameCheck] = useState<boolean>(true);
     const [addressCheck, setAddressCheck] = useState<boolean>(true);
     const [nationCheck, setNationCheck] = useState<boolean>(true);
@@ -28,11 +30,11 @@ function SignUpComponent() {
             case "email": setEmail(e.target.value); break;
             case "password": setPassword(e.target.value); break;
             case "repeatPassword": {
-                password !== e.target.value ? setIsSame(false) : setIsSame(true); 
+                setrepeatPw(e.target.value);
+                setRepeatCheck(password === e.target.value);
                 break;
             }
             case "name": setName(e.target.value); break;
-            //case "birth": setBirth(moment(e.target.value).format("YYYY년 MM월 DD일")); break;
             case "address": setAddress(e.target.value); break;
             case "gender": setGender(e.target.value); break;
             case "nation": setNation(e.target.value); break;
@@ -41,42 +43,41 @@ function SignUpComponent() {
 
     const EmailInput:any = useRef();
     const PasswordInput:any = useRef();
+    const RepeatPasswordInput:any = useRef();
     const NameInput:any = useRef();
     const AddressInput:any = useRef();
     const NationInput:any = useRef();
-    
-    // focus 이동 
-    const moveFocus =  () => {
-        if (!emailCheck) EmailInput.current.focus();
-        else if (!pwCheck) PasswordInput.current.focus();
-        else if (!nameCheck) NameInput.current.focus();
-        else if (!addressCheck) AddressInput.current.focus();
-        else if (!nationCheck) NationInput.current.focus();
-    }
 
     // sign up 버튼 눌렀을 때  
-    const SignUpCheck = async() => {
-        email === "" ? setEmailCheck(false) : setEmailCheck(true);
-        password === "" ? setPwCheck(false) : setPwCheck(true);
-        name === "" ? setNameCheck(false) : setNameCheck(true);
-        address === "" ? setAddressCheck(false) : setAddressCheck(true);
-        nation === "" ? setNationCheck(false) : setNationCheck(true);
+    const SignUpCheck = () => {
+        setEmailCheck(!(email === ""));
+        setPwCheck(!(password === ""));
+        setRepeatCheck(!(repeatPw === ""));
+        setNameCheck(!(name === ""));
+        setAddressCheck(!(address === ""));
+        setNationCheck(!(nation === ""));
 
-        moveFocus();   // todo: 동기처리 필요 
-        // else await SignUp();   // 모두 통과하면 
+        // focus 이동  
+        if (email === "") EmailInput.current.focus();
+        else if (password === "") PasswordInput.current.focus();
+        else if (!repeatCheck || repeatPw === "") RepeatPasswordInput.current.focus();
+        else if (name === "") NameInput.current.focus();
+        else if (address === "") AddressInput.current.focus();
+        else if (nation === "") NationInput.current.focus();
+        else SignUp();
     }
 
     const SignUp = () => {
         const response = axios.post(
             "http://15.164.231.10/user/signup",
             {
-                "email": "string",
-                "password": "string",
-                "name": "string",
-                "birthday": "Unknown Type: date",
-                "address": "string",
-                "sex": "string",
-                "nation": "string"
+                "email": email,
+                "password": password,
+                "name": name,
+                "birthday": dayjs(birth).format("YY/MM/DD"),
+                "address": address,
+                "sex": gender,
+                "nation": nation
             },
             {
                 headers: {
@@ -102,28 +103,21 @@ function SignUpComponent() {
                 <S.inputContainer>
                     <S.inputTitle check={emailCheck} >Email *</S.inputTitle>
                     <S.inputBox check={emailCheck} ref={EmailInput} type="email" onChange={changeValue("email")}/>
-
                     <S.inputTitle check={pwCheck}>Password *</S.inputTitle>
                     <S.inputBox check={pwCheck} ref={PasswordInput} type="password" onChange={changeValue("password")}/>
-
-                    <S.inputTitle check={isSame}>Repeat Password *</S.inputTitle>
-                    <S.inputBox check={isSame} type="password" onChange={changeValue("repeatPassword")}/>
-
+                    <S.inputTitle check={repeatCheck}>Repeat Password *</S.inputTitle>
+                    <S.inputBox check={repeatCheck} ref={RepeatPasswordInput} type="password" onChange={changeValue("repeatPassword")}/>
                     <S.inputTitle check={nameCheck}>Name *</S.inputTitle>
                     <S.inputBox check={nameCheck} ref={NameInput} type="name" onChange={changeValue("name")}/>
-
                     <S.inputTitle check={true}>Birth *</S.inputTitle>
-                    <Calendar className="react-calendar" value={birth} />
-
+                    <Calendar className="react-calendar" value={birth} onChange={setBirth} />
                     <S.inputTitle check={addressCheck}>Address *</S.inputTitle>
                     <S.inputBox check={addressCheck} ref={AddressInput} type="address" onChange={changeValue("address")}/>
-
                     <S.inputTitle check={true}>Gender *</S.inputTitle>
                     <S.genderSelectBox onChange={changeValue("gender")} >
                         <option value={"female"} key={"female"}>female</option>
                         <option value={"male"} key={"male"}>male</option>
                     </S.genderSelectBox>
-
                     <S.inputTitle check={nationCheck}>Nation *</S.inputTitle>
                     <S.inputBox check={nationCheck} ref={NationInput} type="nation" onChange={changeValue("nation")}/>
                 </S.inputContainer>
